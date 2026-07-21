8051f1868b0ed371c59b45d5cd0d584e591a92c1
Pedro Casseb <casseb.phcc@gmail.com>
2026-07-21 14:27:13 -0300

    feat(config-demo): add actuator support for dynamic configuration refresh

--- additional/config-management/config-demo/src/main/java/com/info/configdemo/BuildInfoController.java
+++ additional/config-management/config-demo/src/main/java/com/info/configdemo/BuildInfoController.java
@@ -2,13 +2,14 @@ package com.info.configdemo;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @AllArgsConstructor
+@RefreshScope
 public class BuildInfoController {
 
     private BuildInfo buildInfo;
