2dfa1382b96077866f867fb25f6a9ada4c25ada5
Pedro Casseb <casseb.phcc@gmail.com>
2026-07-21 13:52:17 -0300

    feat(config-management): add spring cloud config server and application configurations

---
+++ additional/config-management/config-demo/src/main/java/com/info/configdemo/BuildInfoController.java
@@ -0,0 +1,21 @@
+package com.info.configdemo;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+public class BuildInfoController {
+
+    private BuildInfo buildInfo;
+
+    @GetMapping("/build-info")
+    public String getBuildInfo() {
+        log.info("Build Id: {}, Version: {}, Name: {}", buildInfo.getId(), buildInfo.getVersion(), buildInfo.getName());
+        return "Build ID: " + buildInfo.getId() + ", Version: " + buildInfo.getVersion() + ", Name: " + buildInfo.getName();
+    }
+}
